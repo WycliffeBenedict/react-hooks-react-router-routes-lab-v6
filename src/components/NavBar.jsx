2224e5b40b9c7950980793654d7191fb7898e2ba
Wycliffe <WycliffeBenedict25@gmail.com>
2025-03-11 14:02:03 +0300

    basic routes lab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,20 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+        Home
+      </NavLink>
+      <NavLink to="/directors" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+        Actors
+      </NavLink>
+      <NavLink to="/movies" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+        Movies
+      </NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
